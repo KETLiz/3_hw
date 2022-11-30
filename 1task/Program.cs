3acb340b35fa271d46ac140107f2d90f5387b862
Li <varlamova_e.t@mail.ru>
2022-11-30 12:42:52 +0300

    1t

---
+++ 1task/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] arr = new int[] {1, 3, 3, 7};
+int sum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (i % 2 != 0) sum += arr[i];
+    
+}
+Console.Write(sum);
